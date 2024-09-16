0153a6673d3ccb6f69956e85921676f1a35b9d2d
velyn666 <ni1722650@gmail.com>
2024-09-16 22:49:53 +0900

    json 반환해보기

--- nestJs/code/CF_SNS/src/app.controller.ts
+++ nestJs/code/CF_SNS/src/app.controller.ts
@@ -1,12 +1,26 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+interface Post {
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getPost(): Post {
+    return {
+      author: 'newjibs',
+      title: '집가고싶다',
+      content: '너무 집가고싶다',
+      likeCount: 10000,
+      commentCount: 999,
+    };
   }
 }
