96d00bfb22ae1fc9611f6f4767b92880dec7fb5a
velyn666 <ni1722650@gmail.com>
2024-09-19 22:31:34 +0900

    포스트 목록 가져오는 api

--- nestJs/code/CF_SNS/src/posts/posts.controller.ts
+++ nestJs/code/CF_SNS/src/posts/posts.controller.ts
@@ -3,7 +3,8 @@ import { PostsService } from './posts.service';
 
 //nest g resource 하면 모듈을 만들 수 있다. 간편하네...
 
-interface Post {
+interface PostModel {
+  id: number;
   author: string;
   title: string;
   content: string;
@@ -11,17 +12,43 @@ interface Post {
   commentCount: number;
 }
 
+const posts: PostModel[] = [
+  {
+    id: 1,
+    author: 'newjibs',
+    title: '집가고싶다',
+    content: '집에가고싶어요 너무',
+    likeCount: 9999,
+    commentCount: 9999,
+  },
+  {
+    id: 2,
+    author: 'qwerqwer',
+    title: '졸려요',
+    content: '집에가고싶어요 너무',
+    likeCount: 9999,
+    commentCount: 9999,
+  },
+  {
+    id: 3,
+    author: 'nononokht',
+    title: '잠이와요',
+    content: '아니 너무 졸리네 너무',
+    likeCount: 9999,
+    commentCount: 9999,
+  },
+];
+
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
+  // 모든 post 가져오기
   @Get()
-  getPost(): Post {
-    return {
-      author: 'newjibs',
-      title: '집가고싶다',
-      content: '너무 집가고싶다',
-      likeCount: 10000,
-      commentCount: 999,
-    };
+  getPosts() {
+    return posts;
   }
+  // 특정 post 가져오기
+  // Post 생성하기
+  // Post 수정하기
+  // Post 삭제하기
 }
