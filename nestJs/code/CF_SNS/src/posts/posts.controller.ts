0f66a894351ba7bc41898a4f01c462225773b25d
velyn666 <ni1722650@gmail.com>
2024-09-19 22:40:53 +0900

    404 not found 에러 반환

--- nestJs/code/CF_SNS/src/posts/posts.controller.ts
+++ nestJs/code/CF_SNS/src/posts/posts.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
 import { PostsService } from './posts.service';
 
 //nest g resource 하면 모듈을 만들 수 있다. 간편하네...
@@ -52,7 +52,14 @@ export class PostsController {
   // GET /posts/:id 파라미터로 구분해서 가져온다
   @Get(':id')
   getPost(@Param('id') id: string) {
-    return posts.find((post) => post.id === +id);
+    //find 함수는 값을 찾지 못하면 undifined를 반환한다.
+    const post = posts.find((post) => post.id === +id);
+
+    if (!post) {
+      throw new NotFoundException();
+    }
+
+    return post;
   }
   // Post 생성하기
   // Post 수정하기
