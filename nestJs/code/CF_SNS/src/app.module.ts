ab50f4c6222f75f2f902d758520b53d2a4cf4926
velyn666 <ni1722650@gmail.com>
2024-10-11 20:10:28 +0900

    Post Model Entity

--- nestJs/code/CF_SNS/src/app.module.ts
+++ nestJs/code/CF_SNS/src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostModel } from './posts/entities/posts.entity';
 
 @Module({
   imports: [
@@ -15,7 +16,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       username: 'postgres',
       password: 'postgres',
       database: 'postgres',
-      entities: [],
+      entities: [
+        PostModel,
+      ],
       //nest js에서 작성하는 type orm 코드와 데이터베이스 자동으로 싱크를 맞출것인가.
       //개발 환경에서는 되어 있는 게 편하지만, 프로덕션 환경에서는 db 구조가 마음대로 바뀔 환경이 있어 false 해야함.
       synchronize: true,
