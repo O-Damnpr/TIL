f0e72bc4898542a9c1c215687a3bfbd19444fca6
velyn666 <ni1722650@gmail.com>
2024-10-12 23:15:03 +0900

    postRepository 주입하기

--- nestJs/code/CF_SNS/src/posts/posts.module.ts
+++ nestJs/code/CF_SNS/src/posts/posts.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostModel } from './entities/posts.entity';
+//레포지토리 주입하려면 사용하려는 거 모듈 안에서 postRepository 임포트
 
 @Module({
+  imports: [
+    //forRoot는 type orm 연결설정, forFeature은 모델에 해당하는 레포지토리 주입할 때
+    TypeOrmModule.forFeature([
+      PostModel,
+    ])
+  ],
   controllers: [PostsController],
   providers: [PostsService],
 })
